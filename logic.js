31908ebc315a84c3983fadf50ef8e169e9f76210
GoodBoyNYC <5098496+GoodBoyNYC@users.noreply.github.com>
2024-08-15 15:15:29 -0400

    chore: Add initial HTML structure, CSS styles, and JavaScript logic for a tic-tac-toe game

---
+++ logic.js
@@ -0,0 +1,32 @@
+const updateScreen = (cell) => {
+    cell.textContent = 'X';
+};
+
+const checkBoard = (arr) => {
+    const winConditions = [
+        /*Row*/[0, 1, 2],
+        /*Row*/[3, 4, 5],
+        /*Row*/[6, 7, 8],
+        /*Column*/[0, 3, 6],
+        /*Column*/[1, 4, 7],
+        /*Column*/[2, 5, 8]
+    ];
+    winConditions.forEach(condition => {
+        const [a, b, c] = condition;
+        if(arr[a]== arr[b] && arr[a] == arr[c]){
+            return console.log(arr[a]);
+        }
+    })
+    return console.log('No Win');
+};
+
+const screenController = (function () {
+    const cells = document.querySelectorAll(".cell");
+    cells.forEach(cell => {
+        cell.addEventListener("click", event => {
+            updateScreen(cell);
+            checkBoard(Array.from(cells));
+        })
+    });
+})();
+
